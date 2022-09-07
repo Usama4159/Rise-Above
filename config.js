b2f8a81433e212ffbe68c03b02ce820317cf940d
tere-hon-yawr <100409922+tere-hon-yawr@users.noreply.github.com>
2022-09-07 20:23:22 +0500

    add start

---
+++ config.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql")
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mintey"
+})
+con.connect((err)=>{
+    if(err){
+        console.log("error is connection")
+    }
+})
+module.exports =con
